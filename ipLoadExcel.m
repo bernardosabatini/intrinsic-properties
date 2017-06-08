f5fb53668a5407ab092ee62e92af8a2789238633
Bernardo <bsabatini@hms.harvarde.edu>
2017-06-08 15:23:04 -0400

    First commit

---
+++ ipLoadExcel.m
@@ -0,0 +1,40 @@
+function [ output_args ] = ipLoadExcel( input_args )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+    evalin('base', 'global ipTableNum ipTableTxt ipTableRaw ipTableSize')
+    global ipTableNum ipTableTxt ipTableRaw ipTableSize
+
+    [filename,pathname,~] = uigetfile('*.*','Select excel file with annotations');
+    if isequal(filename,0)
+        disp('User selected Cancel')
+        return
+    else
+        disp(['User selected ', fullfile(pathname, filename)])
+        [ipTableNum, ipTableTxt, ipTableRaw] = xlsread(fullfile(pathname, filename));
+    end
+    
+    aa=cellfun(@isnan, ipTableRaw(1,:), 'un',0);
+    lastCol=find(cell2mat(cellfun(@all, aa, 'un',0)));
+    if isempty(lastCol)
+        lastCol=size(ipTableRaw,2);
+    else
+        lastCol=lastCol(1)-1;
+    end
+    
+    aa=cellfun(@isnan, ipTableRaw(:,1), 'un',0);
+    lastRow=find(cell2mat(cellfun(@all, aa, 'un',0)));
+    if isempty(lastRow)
+        lastRow=size(ipTableRaw,1);
+    else
+        lastRow=lastRow(1)-1;
+    end   
+    disp([num2str(lastCol) ' columns of data loaded for ' ...
+        num2str(lastRow-1) ' cells'])
+    ipTableSize=[lastCol, lastRow];
+
+    
+    
+    
+    
+
---
+++ ipRunAnalysis.m
@@ -0,0 +1,44 @@
+function [ output_args ] = ipRunAnalysis( input_args )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+    global ipTableNum ipTableTxt ipTableRaw ipTableSize
+
+    for rowCounter=2:ipTableSize % the first row has headers
+        
+
+    evalin('base', 'global ipTableNum ipTableTxt ipTableRaw ipTableSize')
+
+    [filename,pathname,~] = uigetfile('*.*','Select excel file with annotations');
+    if isequal(filename,0)
+        disp('User selected Cancel')
+        return
+    else
+        disp(['User selected ', fullfile(pathname, filename)])
+        [ipTableNum, ipTableTxt, ipTableRaw] = xlsread(fullfile(pathname, filename));
+    end
+    
+    aa=cellfun(@isnan, ipTableRaw(1,:), 'un',0);
+    lastCol=find(cell2mat(cellfun(@all, aa, 'un',0)));
+    if isempty(lastCol)
+        lastCol=size(ipTableRaw,2);
+    else
+        lastCol=lastCol(1)-1;
+    end
+    
+    aa=cellfun(@isnan, ipTableRaw(:,1), 'un',0);
+    lastRow=find(cell2mat(cellfun(@all, aa, 'un',0)));
+    if isempty(lastRow)
+        lastRow=size(ipTableRaw,1);
+    else
+        lastRow=lastRow(1)-1;
+    end   
+    disp([num2str(lastCol) ' columns of data loaded for ' ...
+        num2str(lastRow-1) ' cells'])
+    ipTableSize=[lastCol, lastRow];
+
+    
+    
+    
+    
+
